7d6113209b4bb4d3d14807b670bd99c82e9983e2
ValentinFLGT <valentin.folliguet@le-campu-numerique.fr>
2021-01-27 09:20:53 +0100

    Initial commit

---
+++ src/main/java/com/sixt/sixtclient/SixtClientApplication.java
@@ -0,0 +1,13 @@
+package com.sixt.sixtclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixtClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixtClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sixt/sixtclient/SixtClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixt.sixtclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixtClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
